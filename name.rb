0fffaac49547bea1f7a47b342ea51f54321ac869
Rich McGrath <richmcgrath@gmail.com>
2016-02-20 23:01:32 -0500

    added new method full_name_with_title

--- name.rb
+++ name.rb
@@ -14,11 +14,13 @@ attr_accessor :title, :first_name, :middle_name, :last_name
     @first_name + " " + @middle_name + " " + @last_name
   end
 
+  def full_name_with_title
+    @title + " " + full_name()
+  end
 end
 
 name = Name.new("Mr.", "Rich", "Warren", "McGrath ")
 puts name.title + " " + name.full_name
 
-puts "Title: #{name.title}"
-#name.title = "Dr."
-#puts "Title: #{name.title}"
+spike = Name.new("Mr.", "Spike", "", "Spiegel")
+puts spike.full_name_with_title
