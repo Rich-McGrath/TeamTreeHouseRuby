1834ad96b71a0d8d578cd641ced129e6da075169
Rich McGrath <richmcgrath@gmail.com>
2016-02-21 13:46:20 -0500

    added transactions methods

--- bank_account.rb
+++ bank_account.rb
@@ -1,9 +1,27 @@
+# Bank account
 class BankAccount
-  def initalize(name)
+  attr_reader :name
+
+  def initialize(name)
     @name = name
-    @transacrions = []
+    @transactions = []
+    add_transaction('Beginning Balance', 0)
+  end
+
+  def credit(description, amount)
+    add_transaction(description, amount)
+  end
+
+  def debit(description, amount)
+    add_transaction(description, -amount)
+  end
+
+  def add_transaction(description, amount)
+    @transactions.push(description: description, amount: amount)
   end
 end
 
-  bank_account = BankAccount.new("Rich")
-  puts bank_account.inspect
+bank_account = BankAccount.new('Rich')
+bank_account.credit('Paycheck', 100)
+bank_account.debit('Groceries', 40)
+puts bank_account.inspect
