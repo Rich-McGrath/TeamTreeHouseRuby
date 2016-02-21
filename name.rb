b91d61a85f4c83d5e5b42852fb050916682f7609
Rich McGrath <richmcgrath@gmail.com>
2016-02-20 21:16:33 -0500

    Added attr_accessor

--- name.rb
+++ name.rb
@@ -1,5 +1,7 @@
 class Name
-attr_reader :title, :first_name, :middle_name, :last_name
+attr_reader :first_name, :middle_name, :last_name
+attr_accessor :title
+
 
   def initialize(title, first_name, middle_name, last_name)
     @title = title
@@ -15,3 +17,7 @@ puts name.title + " " +
      name.first_name + " " +
      name.middle_name + " " +
      name.last_name
+
+puts "Title: #{name.title}"
+name.title = "Dr."
+puts "Title: #{name.title}"
