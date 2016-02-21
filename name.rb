d673ebb2f177cb2a2497a179e2629ba4082f6b67
Rich McGrath <richmcgrath@gmail.com>
2016-02-20 23:50:52 -0500

    to_s method

--- name.rb
+++ name.rb
@@ -19,10 +19,19 @@ attr_accessor :title, :first_name, :middle_name, :last_name
   def full_name_with_title
     @title + " " + full_name()
   end
+
+  def to_S
+    full_name_with_title
+  end
+
 end
 
 name = Name.new("Mr.", "Rich", "Warren", "McGrath ")
-puts name.title + " " + name.full_name
+puts name.full_name_with_title
+#puts name
+#puts name.title + " " + name.full_name
 
 spike = Name.new("Mr.", "Spike", "", "Spiegel")
 puts spike.full_name_with_title
+#puts spike
+#puts spike.full_name_with_title
