849b2e2114f51d67061df79a0dbd861122f541bb
Rich McGrath <richmcgrath@gmail.com>
2016-03-01 18:33:23 -0500

    created find index method and mark complete method

--- todo_list.rb
+++ todo_list.rb
@@ -13,24 +13,41 @@ class TodoList
   end
 
   def remove_item(name)
+    if index = find_index(name)
+      todo_items.delete_at(index)
+      return true
+    else
+      return false
+    end
+  end
+
+  def mark_complete(name)
+    if index = find_index(name)
+      todo_items[index].mark_complete!
+      return true
+    else
+      return false
+    end
+  end
+
+  def find_index(name)
     index = 0
     found = false
     todo_items.each do |todo_item|
       if todo_item.name == name
         found = true
       end
-        if found
-          break
-        else
-          index += 1
-        end
-      end
       if found
-        todo_items.delete_at(index)
-        return true
+        break
       else
-        return false
+        index += 1
       end
+    end
+    if found
+      return index
+    else
+      return nil
+    end
   end
 end
 
@@ -42,5 +59,9 @@ puts todo_list.inspect
 
 if todo_list.remove_item("Eggs")
   puts "Eggs were removed from the list."
+end
+
+if todo_list.mark_complete("Milk")
+  puts "Milk was marke as complete."
 end
 puts todo_list.inspect
