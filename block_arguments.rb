4b1998212d553d5b4d7062b9364c7f102f36e3dd
Rich McGrath <richmcgrath@gmail.com>
2016-03-02 18:20:35 -0500

    calling the block using the call method

--- block_arguments.rb
+++ block_arguments.rb
@@ -1,9 +1,9 @@
 def get_name
-    print "Enter your name:"
+  print "Enter your name:"
     name = gets.chomp
     yield name
     name
-  end
+end
 
   get_name do |your_name|
     puts "That's a cool name, #{your_name}!"
