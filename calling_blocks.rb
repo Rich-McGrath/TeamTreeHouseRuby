d4b05af74c7be366a457e3ff5a574ec78c383acf
Rich McGrath <richmcgrath@gmail.com>
2016-03-02 18:22:03 -0500

    Example use of block_given? method

--- calling_blocks.rb
+++ calling_blocks.rb
@@ -1,11 +1,11 @@
-def get_name( &block)
-  #print prompt + ": "
+def get_name(prompt, &block)
+  print prompt
   name = gets.chomp
-  block.call(name)
+  block.call(name) if block_given?
   name
 end
 
-my_name = get_name do |your_name|
+my_name = get_name ("Name: ") do |your_name|
   puts "That's a cool name, #{your_name}!"
 end
 
