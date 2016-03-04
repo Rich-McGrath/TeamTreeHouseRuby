e04d7e9c91a211f33d0ee60158dbf7e3e1bb753e
Rich McGrath <richmcgrath@gmail.com>
2016-03-03 21:29:57 -0500

    Added monster actions methods

--- monster.rb
+++ monster.rb
@@ -1,15 +1,51 @@
 class Monster
-  attr_reader :name
+  attr_reader :name, :actions
 
   def initialize(name)
     @name = name
+    @actions = {
+      screams: 0,
+      scares: 0,
+      hides: 0,
+      runs: 0
+    }
   end
 
   def say(&block)
     print "#{name} says... "
-    block.call(name)
+    yield
+  end
+
+  def scream(&block)
+    actions[:screams] += 1
+    print "#{name} screams!"
+    yield
+  end
+
+  def scare(&block)
+    actions[:scares] =+1
+    print "#{name} scares you!"
+    yield
+  end
+
+  def hide(&block)
+    actions[:hides] =+1
+    print "#{name} hides!"
+    yield
+  end
+
+  def run(&block)
+    actions[:runs] =+1
+    print "#{name} runs"
+    yield
   end
 end
 
 monster = Monster.new("Fluffy")
 monster.say { puts "Welcome to my home."}
+monster.scream { puts " BOO!" }
+monster.scare { puts " Go away!" }
+monster.run { puts "Going to get you!"}
+monster.hide { puts "Running away and hiding?"}
+
+puts monster.actions
